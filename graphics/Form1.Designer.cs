c5587b2fef2653d08bbcb9394a297e2c72652e41
Geliron12 <65118717+Geliron12@users.noreply.github.com>
2020-05-14 19:35:48 +0500

    Add files via upload

---
+++ graphics/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace graphics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxw1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxw0 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_xmax = new System.Windows.Forms.TextBox();
+            this.textBox_xmin = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_1
+            // 
+            this.button_1.Location = new System.Drawing.Point(776, 239);
+            this.button_1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button_1.Name = "button_1";
+            this.button_1.Size = new System.Drawing.Size(220, 95);
+            this.button_1.TabIndex = 0;
+            this.button_1.Text = "построить";
+            this.button_1.UseVisualStyleBackColor = true;
+            this.button_1.Click += new System.EventHandler(this.Построить_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(896, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "w1=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(771, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(209, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введите параметры:";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // textBoxw1
+            // 
+            this.textBoxw1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBoxw1.Location = new System.Drawing.Point(952, 142);
+            this.textBoxw1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxw1.Name = "textBoxw1";
+            this.textBoxw1.Size = new System.Drawing.Size(59, 22);
+            this.textBoxw1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(771, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "w0=";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // textBoxw0
+            // 
+            this.textBoxw0.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBoxw0.Location = new System.Drawing.Point(831, 142);
+            this.textBoxw0.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxw0.Name = "textBoxw0";
+            this.textBoxw0.Size = new System.Drawing.Size(59, 22);
+            this.textBoxw0.TabIndex = 7;
+            this.textBoxw0.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.pictureBox1.Location = new System.Drawing.Point(89, 50);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(533, 492);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(897, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "xmax=";
+            this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(771, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "xmin=";
+            // 
+            // textBox_xmax
+            // 
+            this.textBox_xmax.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox_xmax.Location = new System.Drawing.Point(971, 201);
+            this.textBox_xmax.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox_xmax.Name = "textBox_xmax";
+            this.textBox_xmax.Size = new System.Drawing.Size(59, 22);
+            this.textBox_xmax.TabIndex = 10;
+            // 
+            // textBox_xmin
+            // 
+            this.textBox_xmin.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox_xmin.Location = new System.Drawing.Point(829, 201);
+            this.textBox_xmin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox_xmin.Name = "textBox_xmin";
+            this.textBox_xmin.Size = new System.Drawing.Size(59, 22);
+            this.textBox_xmin.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(771, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "A=";
+            this.label6.Click += new System.EventHandler(this.Label6_Click);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBoxA.Location = new System.Drawing.Point(829, 102);
+            this.textBoxA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(59, 22);
+            this.textBoxA.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(209, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(313, 25);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "график вида : Asin(w1*t)sin(w0*t)";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(776, 468);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 74);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Построить по точкам";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox1.Location = new System.Drawing.Point(799, 408);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 30);
+            this.textBox1.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(782, 346);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(144, 25);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Путь к файлу:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 596);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox_xmin);
+            this.Controls.Add(this.textBox_xmax);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxw0);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxw1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button_1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Построение графика";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxw1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxw0;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_xmax;
+        private System.Windows.Forms.TextBox textBox_xmin;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ graphics/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace graphics
+{
+    public partial class Form1 : Form
+    {
+        public class point : IComparable<point>
+        {
+            public double x, y;
+            public int CompareTo(point example)
+            {
+                return this.x.CompareTo(example.x);
+            }
+            public point(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+            public int acc = 1000;
+        public double w0,w1,xmin, xmax, A;
+        Pen black = new Pen(Color.Black);
+        Pen blue = new Pen(Color.Blue);
+        Pen gray = new Pen(Color.Gray);
+        Font drawFont = new Font("Arial", 10);
+        SolidBrush drawBrush = new SolidBrush(Color.Black);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Построить_Click(object sender, EventArgs e)
+        {
+                Bitmap bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics graphic = Graphics.FromImage(bitmap);
+                pictureBox1.Image = bitmap;
+            try { 
+                w0 = Convert.ToDouble(textBoxw0.Text);
+                w1 = Convert.ToDouble(textBoxw1.Text);
+                xmin = Convert.ToDouble(textBox_xmin.Text);
+                xmax = Convert.ToDouble(textBox_xmax.Text);
+                A = Convert.ToDouble(textBoxA.Text);
+
+                double xmaxr = Math.Max(Math.Abs(xmin), Math.Abs(xmax));              
+                var answer = new point(0, 0);
+                answer = DrawGrid(xmaxr, Math.Abs(A), graphic);
+                double xpointmax, ypointmax;
+                xpointmax = answer.x;
+                if (xmax <=xmin)
+                {
+                    xpointmax = 0;
+                }
+                ypointmax = answer.y;
+                double kx = pictureBox1.Width / (xpointmax*2);
+                double ky = pictureBox1.Height / (ypointmax*2);
+
+                double[] arrx = new double[acc];
+                arrx[0] = xmin;
+                double newdelta = (xmax - xmin) / acc;
+                for (int i = 1; i < acc; i++) {
+                    arrx[i] = arrx[i - 1] + newdelta;
+                }
+                for (int i = 0; i < acc - 1; i++) {
+                    graphic.DrawLine(blue, pictureBox1.Width / 2 + (float)(arrx[i] * kx),
+                        pictureBox1.Height / 2 - (float)(fun(arrx[i], A, w0, w1) * ky),
+                        pictureBox1.Width / 2 + (float)(arrx[i + 1] * kx),
+                        pictureBox1.Height / 2 - (float)(fun(arrx[i + 1], A, w0, w1) * ky));
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверно введены данные!", "Ошибка!",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private double fun(double x, double A, double w0, double w1) {
+            return A * Math.Sin(w0 * x) * Math.Sin(w1 * x);
+        }
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+                Bitmap bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics graphic = Graphics.FromImage(bitmap);
+                pictureBox1.Image = bitmap;
+            try { 
+                string path = textBox1.Text;
+                int n;
+                double tempx, tempy;
+                var points = new List<point>();
+                using (StreamReader str = new StreamReader(path))
+                {
+                    n = Convert.ToInt32(str.ReadLine());
+                    for (int i = 0; i < n; i++)
+                    {
+                        tempx = Convert.ToDouble(str.ReadLine());
+                        tempy = Convert.ToDouble(str.ReadLine());
+                        points.Add(new point(tempx, tempy));
+                    }
+                }
+                var another = points.OrderBy(s => s);
+                var supportive = another.Select(s => Math.Abs(s.y));
+                double xpointmax, ypointmax, kx, ky, xmaxr, ymaxr;
+                xmaxr = Math.Max(Math.Abs(another.Last().x), Math.Abs(another.First().x));
+                ymaxr = supportive.Max();
+                var answer = new point(0, 0);
+                answer = DrawGrid(xmaxr, ymaxr, graphic);
+                xpointmax = answer.x;
+                if (xmaxr == 0)
+                {
+                    xpointmax = 0;
+                }
+                ypointmax = answer.y;
+                kx = pictureBox1.Width / (xpointmax - (-1) * xpointmax);
+                ky = pictureBox1.Height / (ypointmax - (-1) * ypointmax);
+                point temp1 = new point(0, 0);
+                point temp2 = new point(0, 0);
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    temp1 = another.Skip(i).First();
+                    temp2 = another.Skip(i + 1).First();
+
+                    graphic.DrawLine(blue, pictureBox1.Width / 2 + (float)(temp1.x * kx),
+                           pictureBox1.Height / 2 - (float)(temp1.y * ky),
+                           pictureBox1.Width / 2 + (float)(temp2.x * kx),
+                           pictureBox1.Height / 2 - (float)(temp2.y * ky));
+                    temp1 = another.Skip(i).First();
+                    temp2 = another.Skip(i + 1).First();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверно введены данные!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+        public point DrawGrid(double xmaxr, double ymaxr,Graphics graphic) {
+            double xpointmax = 0.01;
+            double ypointmax = 0.01;
+            double deltax = 0.002;
+            double deltay = 0.002;
+            int koef = 5;
+            while (xpointmax < xmaxr)
+            {
+                if (xpointmax > 1 && koef == 5)
+                {
+                    koef = 2;
+                }
+                xpointmax *= koef;
+                deltax *= koef;
+            }
+            koef = 5;
+            while (ypointmax < Math.Abs(ymaxr))
+            {
+                if (ypointmax > 1&&koef ==5)
+                {
+                    koef = 2;
+                }
+                ypointmax *= koef;
+                deltay *= koef;
+
+            }
+            double kx = pictureBox1.Width / (xpointmax * 2);
+            double ky = pictureBox1.Height / (ypointmax * 2);
+            double fx = -1 * xpointmax;
+            double fy = -1 * ypointmax;
+
+            while (fx < xpointmax)
+            {
+                graphic.DrawString(Convert.ToString(Math.Round(fx, 4)), drawFont, drawBrush, (float)(pictureBox1.Width / 2 +
+                    fx * kx), pictureBox1.Height / 2 + 5);
+                fx += deltax;
+            }
+            while (fy < ypointmax)
+            {
+                if (Math.Round(fy, 4) != 0)
+                {
+                    graphic.DrawString(Convert.ToString((-1)*Math.Round(fy, 4)), drawFont, drawBrush, pictureBox1.Width / 2 + 5,
+                        (float)(pictureBox1.Height / 2 + fy * ky));
+                }
+                fy += deltay;
+            }
+            for (int i = 0; i < pictureBox1.Width; i += 40)
+            {
+                graphic.DrawLine(gray, i, 0, i, pictureBox1.Height);
+                graphic.DrawLine(black, i, pictureBox1.Height / 2 - 5, i, pictureBox1.Height / 2 + 5);
+                graphic.DrawLine(gray, 0, i, pictureBox1.Width, i);
+                graphic.DrawLine(black, pictureBox1.Width / 2 - 5, i, pictureBox1.Width / 2 + 5, i);
+            }
+            graphic.DrawLine(black, 0, pictureBox1.Height / 2, pictureBox1.Width, pictureBox1.Height / 2);
+            graphic.DrawLine(black, pictureBox1.Width - 10, pictureBox1.Height / 2 - 10, pictureBox1.Width, pictureBox1.Height / 2);
+            graphic.DrawLine(black, pictureBox1.Width - 10, pictureBox1.Height / 2 + 10, pictureBox1.Width, pictureBox1.Height / 2);
+            graphic.DrawLine(black, pictureBox1.Width / 2, 0, pictureBox1.Width / 2, pictureBox1.Height);
+            graphic.DrawLine(black, pictureBox1.Width / 2 - 10, 10, pictureBox1.Width / 2, 0);
+            graphic.DrawLine(black, pictureBox1.Width / 2 + 10, 10, pictureBox1.Width / 2, 0);
+            return ( new point(xpointmax, ypointmax));
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
